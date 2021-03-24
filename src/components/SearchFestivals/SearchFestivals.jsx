d4a514da4520f7979c5a1769203713142c36b489
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2021-03-24 23:08:20 +0100

    displaying cards on searchfestvals

--- src/components/SearchFestivals/SearchFestivals.jsx
+++ src/components/SearchFestivals/SearchFestivals.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import { Container, Search, Dropdown , Label, Grid } from 'semantic-ui-react';
+import { Container, Search, Dropdown , Label, Grid, Button, Modal, Card, Image } from 'semantic-ui-react';
 import usersAndFestivalsService from '../../service/usersAndFestivalsService';
+import './SearchFestivals.css'
 import 'semantic-ui-css/semantic.min.css';
 
 
@@ -25,37 +26,34 @@ const SearchFestivals = (props) => {
 
     const [country, setCountry] = useState('');
     const [genre, setGenre] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const [festivals, setFestivals] = useState([]);
 
 
     const handleCountrySearchChange = (e) => {
         e.preventDefault();
         setCountry(e.target.value);
-
-        if(genre !== ''){
-            let rez = usersAndFestivalsService.searchFestivals(country, genre).then(resp => {
-                console.log(resp.data);
-            });
-        }
     }
 
     const handleGenreChange = (e, data) => {
         e.preventDefault();
-
         const theGenre = data.value;
-
         setGenre(theGenre);
-        console.log(data.value);
+    }
 
-        console.log(genre);
-
-        if(country !== ''){
-            let rez = usersAndFestivalsService.searchFestivals(country, genre).then(resp => {
+    const handleSearchButtonClick = (e) => {
+        if(country !== '' && genre !== ''){
+            usersAndFestivalsService.searchFestivals(country, genre).then(resp => {
                 console.log(resp.data);
+                setFestivals(resp.data);
             });
+        }
+        else{
+            setShowModal(true);
         }
     }
 
-    
+
 
     return (
         <>
@@ -63,30 +61,67 @@ const SearchFestivals = (props) => {
             <Container>
 
                 <Grid divided='vertically'>
-                    <Grid.Row columns={2}>
+                    <Grid.Row columns={3}>
 
-                    <Grid.Column>
-                    <Label>Search country:</Label>
-                    <Search
-                        type='text'
-                        size='big'
-                        showNoResults={false}
-                        onSearchChange={handleCountrySearchChange}
-                    />
-                    </Grid.Column>
+                        <Grid.Column>
+                            <Label className='search-elements'>Search country:</Label>
+                            <Search
+                                type='text'
+                                size='big'
+                                showNoResults={false}
+                                onSearchChange={handleCountrySearchChange}
+                                className='search-bar search-elements'
+                            />
+                    
+                            <Dropdown
+                                placeholder='Select Genre'
+                                fluid
+                                search
+                                selection
+                                options={genreOptions}
+                                onChange={handleGenreChange}
+                                className='genre-dropdown search-elements'
+                            />
 
-                    <Grid.Column>
-                    <Dropdown
-                        placeholder='Select Genre'
-                        fluid
-                        search
-                        selection
-                        options={genreOptions}
-                        onChange={handleGenreChange}
-                    />
-                    </Grid.Column>
+                            <Button 
+                                secondary
+                                onClick={handleSearchButtonClick}
+                                size='big'
+                                className='search-elements'
+                            >
+                                SEARCH
+                            </Button>
+
+                            <Modal open={showModal}>
+                                <Modal.Content>
+                                    Country or genre is empty!
+                                    <Button
+                                     onClick={()=>{setShowModal(false)}}
+                                     className='modal-btn'>
+                                        Ok
+                                    </Button>
+                                </Modal.Content>
+                            </Modal>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            
+                            {
+                                festivals.map(f => {
+                                    return <Card>
+                                        <Image src={f.image} wrapped ui={false}/>
+                                        <Card.Content>
+                                            <Card.Description>
+                                                {f.name}
+                                            </Card.Description>
+                                        </Card.Content>
+                                    </Card>
+                                })
+                            }
+                        </Grid.Column>
 
                     </Grid.Row>
+
                 </Grid>
                 
             </Container>
