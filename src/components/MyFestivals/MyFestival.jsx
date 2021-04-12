8374714a071b612a136e6bc7e79897a5a591ecfd
Andrej Hristov <andrejhris23@gmail.com>
2021-04-12 23:35:51 +0200

    feat: create myfestival component (not done)"

---
+++ src/components/MyFestivals/MyFestival.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+const MyFestival = () => {
+    
+    useEffect(() => {
+
+    }, [])
+    
+    const fetchUserFestivals = () => {
+        // TODO : call axios authService
+    }
+
+    return(
+        <div></div>
+    );
+}
